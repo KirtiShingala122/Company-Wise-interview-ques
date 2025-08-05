a11e3b93ced5cd776db6c35468dd0a9e019e4eee
KirtiShingala122 <kirtishingala122@gmail.com>
2025-08-05 23:43:57 +0530

    Create delete_middle_of_linked_list.cpp

---
+++ Morgan_stanley/delete_middle_of_linked_list.cpp
@@ -0,0 +1,40 @@
+//Delete Middle of Linked List
+/* in this we reached till middle point then  */
+/* Link list Node:
+
+struct Node
+{
+    int data;
+    struct Node* next;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+*/
+
+// Deletes middle of linked list and returns head of the modified list
+class Solution {
+  public:
+    Node* deleteMid(Node* head) {
+        if (!head) return nullptr;
+        if (!head->next) 
+        { 
+            delete head;
+            return nullptr;
+        }
+        Node* fast=head;
+        Node* slow=head;
+        Node* prev=nullptr;
+        while(fast && fast->next){
+           prev=slow;
+           slow = slow->next;
+           fast=fast->next->next;
+        }
+        prev->next=slow->next;
+        delete slow;
+        return head;
+    }
+};
